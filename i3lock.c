e171d43313812da4bd172575c5d4e6eb5c966c0b
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2016-01-30 16:02:23 +0100

    clang-format

--- i3lock.c
+++ i3lock.c
@@ -1090,7 +1090,7 @@ int main(int argc, char *argv[]) {
     }
     free(image_path);
 
-    if(blur > 0 && img != NULL) {
+    if (blur > 0 && img != NULL) {
         blur_surface(img, blur);
     }
 
